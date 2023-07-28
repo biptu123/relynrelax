478755ae7d0c5367fbdd2dd54976980c477e7e9b
Biptu <c.biptu.pc.mail@gmail.com>
2023-07-28 03:05:22 -0700

    Update Upload.js

--- routes/Upload.js
+++ routes/Upload.js
@@ -8,7 +8,7 @@ const router = express.Router();
 // Set up Multer storage configuration
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'uploads/');
+        cb(null, 'https://github.com/biptu123/relynrelax/upload/');
     },
     filename: function (req, file, cb) {
         cb(null, Date.now() + '-' + file.originalname);
