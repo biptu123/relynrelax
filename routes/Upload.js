1e60856711b6f992fdff3f4a894b12d6536bbe3f
Biptu <c.biptu.pc.mail@gmail.com>
2023-07-29 04:59:05 -0700

    changes

--- routes/Upload.js
+++ routes/Upload.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 // Set up Multer storage configuration
 const storage = multer.diskStorage({
-    destination: path.join(__dirname, './uploads'),
+    destination: path.join(__dirname, '../uploads'),
     filename: function (req, file, cb) {
         cb(null, Date.now() + '-' + file.originalname);
     },
